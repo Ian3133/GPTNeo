6bdc97aa3eb49f213c9f6940d5c40989dbc03d96
Ian <iant@bu.edu>
2023-08-10 15:26:06 -0400

    loop

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print(5)
+from transformers import pipeline 
+generator = pipeline('text-generation', model='EleutherAI/gpt-neo-2.7B')
+
+while True:
+    prompt = input("Ask anything: (or type 'break' to exit) \n") 
+    if prompt =="break":
+        break
+    res = generator((prompt), max_length=50, do_sample=True, temperature=0.8) 
+    print(res[0]['generated_text'])
